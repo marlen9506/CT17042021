41fa39114997ce7875fb0d4ae5109a20ceef63d3
marlene.garcia <marlene.garcia@MGARCIA>
2022-04-16 10:30:52 -0500

    commit inicial

---
+++ Selenium/src/main/java/test/TestSelenium.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSelenium {
+
+	public static void main(String[] args) {
+		String exePath = Paths.get("").toAbsolutePath().toString()+ File.separator + "driver" + File.separator;
+		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
+
+}
---
+++ Selenium/src/test/java/test/NewTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void Test() {
+	  	String exePath = Paths.get("").toAbsolutePath().toString()+ File.separator + "driver" + File.separator;
+		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+  }
+}
